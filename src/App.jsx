a51589047fcc371213da9f1b39625a37191ec65f
rafaelrendonx <rafael_x@hotmail.com>
2024-02-22 13:23:39 -0700

    Pequenos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ export default function App() {
         </h1>
         <p className='font-outfit m-8'>An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
 
-        <div className='bg-eggshell font-outfit m-8'>
-          <h3 className='font-semibold text-dark-raspberry text-lg'>Preparation time</h3>
-          <ul>
-            <li>Total: Approximately 10 minutes</li>
-            <li>Preparation: 5 minutes</li>
-            <li>Cooking: 5 minutes</li>
+        <div className='bg-eggshell font-outfit m-8 p-3 rounded-lg'>
+          <h3 className='font-semibold text-dark-raspberry text-xl m-3'>Preparation time</h3>
+          <ul className='list-disc'>
+            <li className='m-3 pl-3'><strong>Total:</strong> Approximately 10 <br /> minutes</li>
+            <li className='m-3 pl-3'><strong>Preparation:</strong> 5 minutes</li>
+            <li className='m-3 pl-3'><strong>Cooking:</strong> 5 minutes</li>
           </ul>
         </div>
 
